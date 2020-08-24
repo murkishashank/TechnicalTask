7e0cd36e3cecac29597ff659887ee44a7f509b6f
murkishashank <65018158+murkishashank@users.noreply.github.com>
2020-08-24 18:21:05 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+function validation(){
+	var name =document.getElementById("fname").value;
+	var phone =document.getElementById("phone").value;
+	var email =document.getElementById("email").value;
+	var description =document.getElementById("description").value;
+	//alert("Hello " + fname + " your form has been submitted.");
+	alert("Hello " + name + " your form has been submitted.");
+//	alert("Hello");
+}
